b550ad8157d9696984901fba38cb17692036fdd6
Matt-Fr <matthieu_franck@orange.fr>
2023-08-27 13:49:46 +0200

    started the navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+  return (
+    <header className=" flex justify-between">
+      <button>email</button>
+      <button>logo</button>
+      <button
+        className="flex flex-col h-12 w-12 border-2 border-black rounded justify-center items-center group"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+        <div
+          className={`${genericHamburgerLine} ${
+            isOpen
+              ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+              : "opacity-50 group-hover:opacity-100"
+          }`}
+        />
+      </button>
+    </header>
+  );
+};
+
+export default Navbar;
