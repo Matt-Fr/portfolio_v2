dac552dc2e1df7bc061c271e0e99125fa92418c2
Matt-Fr <matthieu_franck@orange.fr>
2023-08-27 17:21:49 +0200

    add sections

---
+++ app/components/section/SectionPresentation.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SectionPresentation = () => {
+  return (
+    <section>
+      <div>
+        <h1>Matthieu Franck</h1>
+        <h2>Je suis développeur Front-end</h2>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default SectionPresentation;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import SectionPresentation from "./components/section/SectionPresentation";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
+      <main>
+        <SectionPresentation></SectionPresentation>
+      </main>
     </>
   );
 }
