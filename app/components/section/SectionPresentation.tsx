96e0b6a3d39decd0cb72841573ebce58538c951a
Matt-Fr <matthieu_franck@orange.fr>
2023-08-28 09:04:27 +0200

    added my pic

--- app/components/section/SectionPresentation.tsx
+++ app/components/section/SectionPresentation.tsx
@@ -1,15 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import authorPic from "@/app/assets/picAuthor.jpg";
 
 const SectionPresentation = () => {
   return (
-    <section className=" m-8 ">
+    <section className=" m-8 flex flex-col items-center ">
       <div className="flex flex-col items-center">
         <h1 className=" text-3xl">Matthieu Franck</h1>
         <h2 className="[text-shadow:_2px_3px_0_rgb(128_244_220_/100%)] text-2xl ">
           Développeur Front-end
         </h2>
-        <p>
+        <p className=" my-5">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat illo
           ex est quos recusandae at nostrum enim cum? Porro, repellat.
         </p>
@@ -28,7 +30,13 @@ const SectionPresentation = () => {
           </Link>
         </div>
       </div>
-      <div></div>
+      <div className=" w-3/4 h-96 overflow-hidden flex justify-center border-2 rounded-2xl  shadow-black ease-out duration-300 hover:translate-x-1 hover:translate-y-1 hover:shadow-none">
+        <Image
+          alt="author picture"
+          src={authorPic}
+          className="object-cover"
+        ></Image>
+      </div>
     </section>
   );
 };
