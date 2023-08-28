f9e4b665f2f60cf1ec14d20a4971841d3b0d9f31
Matt-Fr <matthieu_franck@orange.fr>
2023-08-28 11:53:54 +0200

    fixed responsive

--- app/components/section/SectionPresentation.tsx
+++ app/components/section/SectionPresentation.tsx
@@ -6,9 +6,9 @@ import authorPic from "@/app/assets/picAuthor.jpg";
 const SectionPresentation = () => {
   return (
     <section className=" m-8 md:m-10 flex flex-col items-center md:flex-row lg:m-14   ">
-      <div className="flex flex-col items-center lg:w-1/2">
+      <div className="flex flex-col items-center justify-center md:w-1/2 md:items-start ">
         <h1 className=" text-3xl md:text-5xl">Matthieu Franck</h1>
-        <h2 className="[text-shadow:_2px_3px_0_rgb(128_244_220_/100%)] text-2xl md:text-4xl">
+        <h2 className="[text-shadow:_2px_3px_0_rgb(128_244_220_/100%)] text-2xl md:text-3xl lg:text-4xl">
           Développeur Front-end
         </h2>
         <p className=" my-5 md:text-lg">
@@ -31,7 +31,7 @@ const SectionPresentation = () => {
         </div>
       </div>
       <div className=" w-full md:w-1/2 flex justify-center items-center h-full ">
-        <div className="overflow-hidden border-2 rounded-2xl  shadow-black ease-out duration-300 hover:translate-x-1 hover:translate-y-1 hover:shadow-none  w-3/4 h-full">
+        <div className="overflow-hidden border-2 rounded-2xl  shadow-black ease-out duration-300 hover:translate-x-1 hover:translate-y-1 hover:shadow-none w-3/4 max-h-96 flex justify-center items-center max-w-sm ">
           <Image
             alt="author picture"
             src={authorPic}
