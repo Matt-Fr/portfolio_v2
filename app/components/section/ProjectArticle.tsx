67634ca04d76a58e8799a8b4fbda0b2439eb23fa
Matt-Fr <matthieu_franck@orange.fr>
2023-08-31 10:02:46 +0200

    add all elements in project

--- app/components/section/ProjectArticle.tsx
+++ app/components/section/ProjectArticle.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 interface project {
@@ -10,10 +11,46 @@ interface project {
   linkGithub?: string;
 }
 
-const ProjectArticle: React.FC<project> = ({ title }) => {
+const ProjectArticle: React.FC<project> = ({
+  title,
+  picture,
+  id,
+  descriptionFr,
+  tags,
+  demo,
+  linkGithub,
+}) => {
   return (
     <article>
-      <h1>{title}</h1>
+      <Image src={picture} alt={`picture of the project ${title}`}></Image>
+      <div>
+        <h2>{title}</h2>
+        {tags &&
+          tags.map((tag, index) => {
+            return <span key={index}>{tag}</span>;
+          })}
+        <p>{descriptionFr}</p>
+        <div>
+          {demo && (
+            <a
+              className="modalProject-info-elements-links-link"
+              href={demo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Demo
+            </a>
+          )}
+          <a
+            className="modalProject-info-elements-links-link"
+            href={linkGithub}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </div>
+      </div>
     </article>
   );
 };
