b0bf57f23a23834cf584e64dc9e2c8743f0444b2
Matt-Fr <matthieu_franck@orange.fr>
2023-09-06 14:59:20 +0200

    center the divs

--- app/components/section/SectionProjects.tsx
+++ app/components/section/SectionProjects.tsx
@@ -4,11 +4,11 @@ import ProjectArticle from "./ProjectArticle";
 
 const SectionProjects = () => {
   return (
-    <section className=" lg:p-14 bg-grey  p-8  justify-items-center">
+    <section className=" lg:p-14 bg-grey  p-8  ">
       <h1 className="[text-shadow:_2px_3px_0_rgb(70_140_247_/50%)] text-2xl md:text-3xl lg:text-4xl my-8">
         mes projets
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-8 justify-items-center">
         {mydata.map((project) => {
           const {
             title,
