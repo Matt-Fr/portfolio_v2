2da65aac959edc8dd7501d7d00f210385ec5cbd4
Matt-Fr <matthieu_franck@orange.fr>
2023-09-06 14:56:14 +0200

    added grid responsive

--- app/components/section/SectionProjects.tsx
+++ app/components/section/SectionProjects.tsx
@@ -4,35 +4,37 @@ import ProjectArticle from "./ProjectArticle";
 
 const SectionProjects = () => {
   return (
-    <section className=" lg:p-14 bg-grey grid grid-cols-1 p-8 gap-y-8 justify-items-center">
-      <h1 className="[text-shadow:_2px_3px_0_rgb(70_140_247_/50%)] text-2xl md:text-3xl lg:text-4xl">
+    <section className=" lg:p-14 bg-grey  p-8  justify-items-center">
+      <h1 className="[text-shadow:_2px_3px_0_rgb(70_140_247_/50%)] text-2xl md:text-3xl lg:text-4xl my-8">
         mes projets
       </h1>
-      {mydata.map((project) => {
-        const {
-          title,
-          id,
-          picture,
-          descriptionFr,
-          tags,
-          demo,
-          linkGithub,
-          logo,
-        } = project;
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-8">
+        {mydata.map((project) => {
+          const {
+            title,
+            id,
+            picture,
+            descriptionFr,
+            tags,
+            demo,
+            linkGithub,
+            logo,
+          } = project;
 
-        return (
-          <ProjectArticle
-            title={title}
-            key={id}
-            picture={picture}
-            descriptionFr={descriptionFr}
-            tags={tags}
-            demo={demo}
-            linkGithub={linkGithub}
-            logo={logo}
-          ></ProjectArticle>
-        );
-      })}
+          return (
+            <ProjectArticle
+              title={title}
+              key={id}
+              picture={picture}
+              descriptionFr={descriptionFr}
+              tags={tags}
+              demo={demo}
+              linkGithub={linkGithub}
+              logo={logo}
+            ></ProjectArticle>
+          );
+        })}
+      </div>
     </section>
   );
 };
