255dc0eff943d8e4c2183932ec8cb1e64c319167
Matt-Fr <matthieu_franck@orange.fr>
2023-09-06 13:14:44 +0200

    changed the layout of the img and modal

--- app/components/section/ProjectArticle.tsx
+++ app/components/section/ProjectArticle.tsx
@@ -51,55 +51,56 @@ const ProjectArticle: React.FC<project> = ({
   };
   return (
     <>
-      <div>
-        {isOpen && (
-          <div className="fixed top-0 left-0 w-full bg-primary h-full flex items-center justify-center  bg-opacity-50 z-50">
-            <div className="bg-white p-6 rounded h-full  shadow-md">
-              <span
-                className="absolute top-0 right-0 mt-2 mr-2 cursor-pointer text-3xl"
-                onClick={closeModal}
-              >
-                &times;
-              </span>
+      {isOpen && (
+        <div className="fixed top-0 left-0 w-full bg-primary h-full flex items-center justify-center  bg-opacity-50 z-50">
+          <div className="bg-white p-8 flex flex-col justify-center  rounded h-full   shadow-md">
+            <span
+              className="absolute top-0 right-0 mt-2 mr-2 cursor-pointer text-3xl"
+              onClick={closeModal}
+            >
+              &times;
+            </span>
+            <div className=" w-full h-64 overflow-hidden">
               <Image
                 src={picture}
                 alt={`picture of the project ${title}`}
-                className=" w-full cursor-pointer  "
+                className=" w-full h-full   object-cover cursor-pointer object-top  "
               ></Image>
-              <h2>{title}</h2>
-              <div className="flex flex-wrap">
-                {tags &&
-                  tags.map((tag, index) => {
-                    return (
-                      <span
-                        className=" text-sm border  rounded-sm py-[2px] px-[5px] mr-2 mb-2"
-                        key={index}
-                      >
-                        {tag}
-                      </span>
-                    );
-                  })}
-              </div>
-              <div className="flex">
-                {demo && (
-                  <a
-                    className=" font-bold text-sm p-2 bg-secondary text-white shadow-black rounded mr-3  ease-out duration-300 hover:translate-x-1 hover:translate-y-1 hover:shadow-none md:text-base"
-                    href={demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Demo
-                  </a>
-                )}
-                <a className=" font-bold text-sm p-2 bg-white shadow-secondary border rounded ease-out duration-300 cursor-pointer hover:translate-x-1 hover:translate-y-1 hover:shadow-none md:text-base">
-                  Github
-                </a>
-              </div>
-              <p className=" text-sm  mb-4 cursor-pointer">{descriptionFr}</p>
             </div>
+            <h2>{title}</h2>
+            <div className="flex flex-wrap">
+              {tags &&
+                tags.map((tag, index) => {
+                  return (
+                    <span
+                      className=" text-sm border  rounded-sm py-[2px] px-[5px] mr-2 mb-2"
+                      key={index}
+                    >
+                      {tag}
+                    </span>
+                  );
+                })}
+            </div>
+            <div className="flex">
+              {demo && (
+                <a
+                  className=" font-bold text-sm p-2 bg-secondary text-white shadow-black rounded mr-3  ease-out duration-300 hover:translate-x-1 hover:translate-y-1 hover:shadow-none md:text-base"
+                  href={demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo
+                </a>
+              )}
+              <a className=" font-bold text-sm p-2 bg-white shadow-secondary border rounded ease-out duration-300 cursor-pointer hover:translate-x-1 hover:translate-y-1 hover:shadow-none md:text-base">
+                Github
+              </a>
+            </div>
+            <p className=" text-sm  mb-4 cursor-pointer">{descriptionFr}</p>
           </div>
-        )}
-      </div>
+        </div>
+      )}
+
       <article className="flex flex-col bg-white border rounded-2xl border-secondary  shadow-black hover:shadow-bigBlack ease-out duration-300 hover:translate-x-[-0.25rem] hover:translate-y-[-0.25rem]  overflow-hidden h-[32rem] max-w-sm">
         <div
           onClick={openModal}
